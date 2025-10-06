25fb39af7cf9c81132895499c240055f87e38dcd
cartieraidan <cartieraidan@gmail.com>
2025-10-06 14:23:32 -0400

    got to work in lab, intervals small but work

--- SYSC3310/lab2/phase-change.c
+++ SYSC3310/lab2/phase-change.c
@@ -1,9 +1,10 @@
+
 #include "msp.h"
 #include <stdbool.h>
 #include <stdio.h>
 
-#define LOOP_INTERVAL_1 4000
-#define LOOP_INTERVAL_2 10000
+#define LOOP_INTERVAL_1 100000
+#define LOOP_INTERVAL_2 300000
 
 static int i;
 static bool LEDstate = true;
@@ -11,7 +12,7 @@ static uint8_t RGBoverflow = ((1<<0)|(1<<1)|(1<<2)); //00000111
 static uint8_t RGBstate = (0<<0);
 static bool loopState = false;
 static uint16_t loopCount = 0;
-static uint16_t loopInterval = LOOP_INTERVAL_1; //default 4 seconds
+static uint16_t loopInterval = (uint16_t) LOOP_INTERVAL_1; //default 4 seconds
 
 
 //comments
@@ -56,19 +57,23 @@ int main() {
 	while (1) {
 		
 		//while both input pins are high
-		while ((P1IN & (uint8_t)(1<<1))&&(P1IN & (uint8_t)(1<<4)))
-		{}
+		//while ((P1IN & (uint8_t)(1<<1))&&(P1IN & (uint8_t)(1<<4)))
+		//{}
 		//debouncing inputs
-		i = 5000;
-		while (i>0){i--;}
+		//i = 5000;
+		//while (i>0){i--;}
 		
 		if (!(P1IN & (uint8_t)(1<<1))) {
+			i = 5000;
+			while (i>0){i--;}
 			//pin 1 pressed, change LED selected
 
 			//RED true, RGB false
 			LEDstate = !LEDstate;
 		}
 		else if (!(P1IN & (uint8_t)(1<<4))) {
+			i = 5000;
+			while (i>0){i--;}
 			//pin 4 pressed
 
             //might suggest debouncing here if testing does not go well
@@ -77,27 +82,27 @@ int main() {
 
 		}
 
-        //continously change states
-        loopCount++;
-        if ((loopCount % loopInterval == 0)&&(loopState)) { //%4000 gussing around 4 seconds
-            
-            loopCount = 0;
-            
-            if (LEDstate) { //RED LED
-                P1OUT ^= (uint8_t)(1<<0);
-            } else { //RGB LED
+			//continously change states
+			loopCount++;
+			if ((loopCount % loopInterval == 0)&&(loopState)) { //%4000 gussing around 4 seconds
+					
+					loopCount = 0;
+					
+					if (LEDstate) { //RED LED
+							P1OUT ^= (uint8_t)(1<<0);
+					} else { //RGB LED
 
-                RGBstate++; //increment state by 1
-                RGBstate &= RGBoverflow; //ensure overflow does not affect other pins
+							RGBstate++; //increment state by 1
+							RGBstate &= RGBoverflow; //ensure overflow does not affect other pins
 
-				if (RGBstate == 0) { //phase change
-					loopInterval = (loopInterval == LOOP_INTERVAL_1) ? LOOP_INTERVAL_2 : LOOP_INTERVAL_1;
-				}
+			if (RGBstate == 0) { //phase change
+				loopInterval = (loopInterval == (uint16_t)LOOP_INTERVAL_1) ? (uint16_t)LOOP_INTERVAL_2 : (uint16_t)LOOP_INTERVAL_1;
+			}
 
-                P2OUT &= (uint8_t)(~((1<<0)|(1<<1)|(1<<2))); //resets pins 0, 1, 2 to 0
-                P2OUT |= RGBstate; //setting new state of pins 0, 1, 2
-            }
-        }
+							P2OUT &= (uint8_t)(~((1<<0)|(1<<1)|(1<<2))); //resets pins 0, 1, 2 to 0
+							P2OUT |= RGBstate; //setting new state of pins 0, 1, 2
+					}
+			}
 		
 	}
 	
