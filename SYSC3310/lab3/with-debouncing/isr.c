9d4107b16649ef7747531a20741e29584a08aee4
cartieraidan <cartieraidan@gmail.com>
2025-10-30 14:57:23 -0400

    Implement debouncing for LED toggle interrupts

--- SYSC3310/lab3/with-debouncing/isr.c
+++ SYSC3310/lab3/with-debouncing/isr.c
@@ -2,25 +2,43 @@
 #include "isr.h"
 #include <stdbool.h>
 
+#define DEBOUNCE_VALUE 50000
+
 volatile static bool LEDstate = true; //default is RED LED = true
 volatile static bool loopLED = false;
-volatile static bool debouced = false;
+volatile static bool debounced = false;
 
 void PORT1_IRQHandler(void) {
-
+    
+    P1IE &= (uint8_t)(~((1<<1)|(1<<4))); //disable interrupt for pin
+    
     T32CONTROL1 |= (uint32_t)(1<<7); //enable timer for debounce
     
-    if ((P1IFG & (uint8_t)(1<<1)) != 0) { //toggling LEDs
-        P1IFG &= (uint8_t)(~(1<<1)); //clearing flag register
-        LEDstate = (LEDstate) ? false : true;
-        
-    } else if ((P1IFG & (uint8_t)(1<<4)) != 0) { //looping through LEDs
-        P1IFG &= (uint8_t)(~(1<<4)); //clearing flag register
-        loopLED = (loopLED) ? false : true; //toggling off and on
+    if (debounced) {
+        if ((P1IFG & (uint8_t)(1<<1)) != 0) { //toggling LEDs
+            P1IFG &= (uint8_t)(~(1<<1)); //clearing flag register
+            LEDstate = (LEDstate) ? false : true;
+            
+        } else if ((P1IFG & (uint8_t)(1<<4)) != 0) { //looping through LEDs
+            P1IFG &= (uint8_t)(~(1<<4)); //clearing flag register
+            loopLED = (loopLED) ? false : true; //toggling off and on
+    
+        }
 
+        debounced = false; //reset for debouncing
     }
 }
 
 void T32_INT1_IRQHandler(void) {
+
+    T32CONTROL1 &= (uint32_t)(~(1<<7)); //disable timer
+
+    T32INTCLR1 = (uint32_t)(1); //clear timer interrupt
+
+    T32LOAD1 = (uint32_t)DEBOUNCE_VALUE; //reset timer count
     
+    debounced = true; //finished debouncing
+
+    P1IE |= (uint8_t)((1<<1)|(1<<4)); //re-enable interrupt for pin
+   
 }
