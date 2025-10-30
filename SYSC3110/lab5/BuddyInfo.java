acb0072bc3ed422332c0bf2312dad8a3d155a15c
cartieraidan <cartieraidan@gmail.com>
2025-10-30 18:41:38 -0400

    Add BuddyInfo class with basic functionality

---
+++ SYSC3110/lab5/BuddyInfo.java
@@ -0,0 +1,32 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo() {
+        this("N/A", "N/A", "N/A");
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo obj = new BuddyInfo("Homer", "Donut", "888-888-8888");
+        System.out.println("Hello " + obj.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
