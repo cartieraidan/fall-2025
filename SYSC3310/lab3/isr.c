cdc7807acb3c1fd2c1caf379bc101ac98a501454
cartieraidan <cartieraidan@gmail.com>
2025-10-30 11:38:58 -0400

    Implement LED state toggling in interrupt handler
    Added LED control logic in PORT1_IRQHandler.

--- SYSC3310/lab3/isr.c
+++ SYSC3310/lab3/isr.c
@@ -1,6 +1,22 @@
 #include <stdint.h>
 #include "isr.h"
+#include <stdbool.h>
+
+static bool LEDstate = true; //default is RED LED = true
+static bool loopLED = false;
+static uint8_t RGBoverflow = ((1<<0)|(1<<1)|(1<<2)); //00000111
+static uint8_t RGBstate = (0<<0);
+static int loopCount = 0;
 
 void PORT1_IRQHandler(void) {
-    
+    //without debouncing
+    if (P1IFG & (uint8_t)(1<<1)) { //toggling LEDs
+        P1IFG &= (uint8_t)(~(1<<1)); //clearing flag register
+        LEDstate = (LEDstate) ? false : true;
+        
+    } else if (P1IFG & (uint8_t)(1<<4)) { //looping through LEDs
+        P1IFG &= (uint8_t)(~(1<<4)); //clearing flag register
+        loopLED = (loopLED) ? false : true; //toggling off and on
+
+    }
 }
