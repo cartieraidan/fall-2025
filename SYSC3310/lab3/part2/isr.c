813a58926b1945b40f171858c047c3d0d4f1bb8d
cartieraidan <cartieraidan@gmail.com>
2025-10-30 16:36:11 -0400

    Implement ISR for LED control and debouncing

---
+++ SYSC3310/lab3/part2/isr.c
@@ -0,0 +1,44 @@
+#include <stdint.h>
+#include "isr.h"
+#include <stdbool.h>
+
+#define DEBOUNCE_VALUE 50000
+
+volatile static bool LEDstate = true; //default is RED LED = true
+volatile bool loopLED = false;
+volatile static bool debounced = false;
+
+void PORT1_IRQHandler(void) {
+    
+    P1IE &= (uint8_t)(~((1<<1)|(1<<4))); //disable interrupt for pin
+    
+    T32CONTROL1 |= (uint32_t)(1<<7); //enable timer for debounce
+    
+    if (debounced) {
+        if ((P1IFG & (uint8_t)(1<<1)) != 0) { //toggling LEDs
+            P1IFG &= (uint8_t)(~(1<<1)); //clearing flag register
+            LEDstate = (LEDstate) ? false : true;
+            
+        } else if ((P1IFG & (uint8_t)(1<<4)) != 0) { //looping through LEDs
+            P1IFG &= (uint8_t)(~(1<<4)); //clearing flag register
+            loopLED = (loopLED) ? false : true; //toggling off and on
+    
+        }
+
+        debounced = false; //reset for debouncing
+    }
+}
+
+void T32_INT1_IRQHandler(void) {
+
+    T32CONTROL1 &= (uint32_t)(~(1<<7)); //disable timer
+
+    T32INTCLR1 = (uint32_t)(1); //clear timer interrupt
+
+    T32LOAD1 = (uint32_t)DEBOUNCE_VALUE; //reset timer count
+    
+    debounced = true; //finished debouncing
+
+    P1IE |= (uint8_t)((1<<1)|(1<<4)); //re-enable interrupt for pin
+   
+}
