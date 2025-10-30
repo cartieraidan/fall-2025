65789932c2a3263dd3bc95d66e1282a88e122660
cartieraidan <cartieraidan@gmail.com>
2025-10-30 12:57:08 -0400

    Refactor interrupt handler condition checks

--- SYSC3310/lab3/with-debouncing/isr.c
+++ SYSC3310/lab3/with-debouncing/isr.c
@@ -7,11 +7,11 @@ volatile static bool loopLED = false;
 
 void PORT1_IRQHandler(void) {
     //without debouncing
-    if (P1IFG & (uint8_t)(1<<1)) { //toggling LEDs
+    if ((P1IFG & (uint8_t)(1<<1)) != 0) { //toggling LEDs
         P1IFG &= (uint8_t)(~(1<<1)); //clearing flag register
         LEDstate = (LEDstate) ? false : true;
         
-    } else if (P1IFG & (uint8_t)(1<<4)) { //looping through LEDs
+    } else if ((P1IFG & (uint8_t)(1<<4)) != 0) { //looping through LEDs
         P1IFG &= (uint8_t)(~(1<<4)); //clearing flag register
         loopLED = (loopLED) ? false : true; //toggling off and on
 
