5976a350d4ea477827f613a607a74c839d639c99
cartieraidan <cartieraidan@gmail.com>
2025-11-03 13:19:49 -0500

    Update isr.c

--- SYSC3310/lab3/isr.c
+++ SYSC3310/lab3/isr.c
@@ -3,12 +3,12 @@
 #include "isr.h"
 #include <stdbool.h>
 
-volatile static bool LEDstate = true; //default is RED LED = true
-volatile static bool loopLED = false;
+extern volatile bool LEDstate = true; //default is RED LED = true
+extern volatile bool loopLED = false;
 
 void PORT1_IRQHandler(void) {
     //without debouncing
-    if ()P1IFG & (uint8_t)(1<<1)) != 0) { //toggling LEDs
+    if ((P1IFG & (uint8_t)(1<<1)) != 0) { //toggling LEDs
         P1IFG &= (uint8_t)(~(1<<1)); //clearing flag register
         LEDstate = (LEDstate) ? false : true;
         
