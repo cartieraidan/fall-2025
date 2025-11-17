c08413d02ce64822c6639f4356cbc77b6ddfbb30
cartieraidan <cartieraidan@gmail.com>
2025-11-17 14:09:42 -0500

    Update isr.c

--- SYSC3310/lab4/isr.c
+++ SYSC3310/lab4/isr.c
@@ -1,13 +1,11 @@
-#include "msp.h"
-#include <stdint.h>
-#include "isr.h"
-#include <stdbool.h>
 
 static volatile bool LEDstate = true; //default RED LED = true
-static uint16_t modes[] = {30000, 60000, 0, 15000}; //regular, slow, off, fast
+static uint16_t modes[] = {300, 600, 0, 150}; //regular, slow, off, fast
 static volatile int index = 0;
 static uint8_t RGBoverflow = ((1<<0)|(1<<1)|(1<<2)); //00000111
 static volatile uint8_t RGBstate = (1<<0);
+static volatile int count = 0;
+static volatile uint16_t mm = 30000;
 
 void PORT1_IRQHandler(void) {
   
@@ -22,10 +20,12 @@ void PORT1_IRQHandler(void) {
         index = (index + 1) % (sizeof(modes) / sizeof(modes[0]));
 
         if (index == 0) { //regular mode
-          TA0CCR0 = modes[index];
+          //TA0CCR0 = modes[index];
+					mm = 30000;
 
         } else if (index == 1) { //slow mode
-          TA0CCR0 = modes[index];
+          //TA0CCR0 = modes[index];
+					mm = 60000;
 
         } else if (index == 2) { //off mode
             TA0CTL &= (uint8_t)(~((1<<4)|(1<<5))); //turn off timer
@@ -36,7 +36,8 @@ void PORT1_IRQHandler(void) {
             }
           
         } else if (index == 3) { //fast mode
-            TA0CCR0 = modes[index]; //set new limit
+            //TA0CCR0 = modes[index]; //set new limit
+						mm = 15000;
     
         }
 
@@ -51,24 +52,28 @@ void PORT1_IRQHandler(void) {
     }
 }
 
-void TA0_0_IRQHandler(void) {
-    if (LEDstate) {
+void TA0_N_IRQHandler(void) {
+  TA0CTL &= (uint8_t)(~(1<<0));
+	count++;
+	//TA0CCR0 = modes[index];
+	TA0CCR0 = mm;
+  
+	if (LEDstate) {
         P1OUT ^= (uint8_t)(1<<0); //toggling RED LED
     } else {
         RGBstate++; //increment state by 1
-	    RGBstate &= RGBoverflow; //ensure overflow does not affect other pins
+				RGBstate &= RGBoverflow; //ensure overflow does not affect other pins
 
-		if (RGBstate == 0) { //after roll over want to initialize to 1 not 0
-			RGBstate++;
-		}
+				if (RGBstate == 0) { //after roll over want to initialize to 1 not 0
+					RGBstate++;
+				}
 	
-	    P2OUT &= (uint8_t)(~((1<<0)|(1<<1)|(1<<2))); //resets pins 0, 1, 2 to 0
-	    P2OUT |= RGBstate; //setting new state of pins 0, 1, 2
+				P2OUT &= (uint8_t)(~((1<<0)|(1<<1)|(1<<2))); //resets pins 0, 1, 2 to 0
+				P2OUT |= RGBstate; //setting new state of pins 0, 1, 2
 
     }
 
 }
-
 
 
 
