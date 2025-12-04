a2e7efa16f9dd7a283e89f8fef79dd1ef613a784
cartieraidan <cartieraidan@gmail.com>
2025-12-03 19:31:05 -0500

    added background images and buttons

--- SYSC3110/milestone5/UnoView.java
+++ SYSC3110/milestone5/UnoView.java
@@ -1,6 +1,7 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class UnoView extends JFrame {
 
@@ -31,22 +32,60 @@ public class UnoView extends JFrame {
         setSize(1000, 800);
 
 
+        //Gets the card png by getting file name from card
+        String cardFileName = "/images/background/player-cards.png";
+        String centerFileName = "/images/background/center-card.png";
+        String topFileName = "/images/background/top.png";
+
+        //Get image icon for card
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(getClass().getResource(cardFileName)));
+        Image cardBack = icon.getImage();
+
+        ImageIcon icon1 = new ImageIcon(Objects.requireNonNull(getClass().getResource(centerFileName)));
+        Image centerBack = icon1.getImage();
+
+        ImageIcon icon2 = new ImageIcon(Objects.requireNonNull(getClass().getResource(topFileName)));
+        Image topBack = icon2.getImage();
+
+
+
+
         //only for testing purposes, will separate
-        playerCards = new JPanel();
-        playerCards.setBackground(Color.LIGHT_GRAY);
+        playerCards = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(cardBack, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        //playerCards.setBackground(Color.LIGHT_GRAY);
         playerCards.setPreferredSize(new Dimension(800, 300));
         playerCards.setLayout(null);
 
-        topPanel = new JPanel(new GridLayout(1, 2));
-        topPanel.setBackground(Color.lightGray);
+        topPanel = new JPanel(new GridLayout(1, 2)) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(topBack, 0, 0, getWidth(), getHeight(), this);
+            }
+        };;
+        //topPanel.setBackground(Color.lightGray);
         topPanel.setPreferredSize(new Dimension(800, 200));
 
-        centerPanel = new JPanel(null);
+        centerPanel = new JPanel(null) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(centerBack, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
         centerPanel.setPreferredSize(new Dimension(400, 400));
         add(centerPanel, BorderLayout.CENTER);
 
         leftPanel = new JPanel();
+        leftPanel.setOpaque(false);
         rightPanel = new JPanel();
+        rightPanel.setOpaque(false);
 
         topPanel.add(leftPanel);
         topPanel.add(rightPanel);
@@ -69,19 +108,63 @@ public class UnoView extends JFrame {
         JPanel panel = getRightPanel(); //JPanel for game controls
 
         //quit JButton
-        JButton quit = new JButton("Quit");
+        String quitFileName = "/images/background/quit-button.png";
+        ImageIcon quitIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource(quitFileName)));
+
+        JButton quit = new JButton(quitIcon);
+        quit.setBorderPainted(false);
+        quit.setContentAreaFilled(false);
+        quit.setFocusPainted(false);
+
         quit.addActionListener(controller);
+
+        quit.setText("Quit");
+        quit.setPreferredSize(new Dimension(
+                quitIcon.getIconWidth(),
+                quitIcon.getIconHeight()
+        ));
+
+
+
         panel.add(quit);
 
         //play JButton
-        play = new JButton("Play");
+        String playFileName = "/images/background/play-button.png";
+        ImageIcon playIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource(playFileName)));
+
+        play = new JButton(playIcon);
+        play.setBorderPainted(false);
+        play.setContentAreaFilled(false);
+        play.setFocusPainted(false);
+
         play.addActionListener(controller);
+
+        play.setText("Play");
+        play.setPreferredSize(new Dimension(
+                playIcon.getIconWidth(),
+                playIcon.getIconHeight()
+        ));
+
         panel.add(play);
 
         //draw JButton
-        draw = new JButton("Draw");
+        String drawFileName = "/images/background/draw-button.png";
+        ImageIcon drawIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource(drawFileName)));
+
+        draw = new JButton(drawIcon);
+        draw.setBorderPainted(false);
+        draw.setContentAreaFilled(false);
+        draw.setFocusPainted(false);
+
         draw.addActionListener(controller);
         draw.setEnabled(false);
+
+        draw.setText("Draw");
+        draw.setPreferredSize(new Dimension(
+                drawIcon.getIconWidth(),
+                drawIcon.getIconHeight()
+        ));
+
         panel.add(draw);
     }
 
